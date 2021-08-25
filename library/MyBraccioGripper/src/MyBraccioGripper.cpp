da2f658229a94f6267ea6225be771826c90dd7ab
ProDanil <43303306+ProDanil@users.noreply.github.com>
2021-08-25 16:42:46 +0300

    Update and rename library/MyBraccio/src/MyBraccio.cpp to library/MyBraccioGripper/src/MyBraccioGripper.cpp

--- library/MyBraccioGripper/src/MyBraccioGripper.cpp
+++ library/MyBraccioGripper/src/MyBraccioGripper.cpp
@@ -17,7 +17,7 @@
  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#include "MyBraccio.h"
+#include "MyBraccioGripper.h"
 
 extern Servo base;
 extern Servo shoulder;
